112b57833e334ef24026b20636970670d1c37b61
qianqing <qian.qing@aliyun.com>
2017-11-03 17:50:55 +0800

    create css and less

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <transition name="router-fade" mode="out-in">
+      <router-view v-wechat-title="$route.meta.title"></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: 'app'
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body{ height: 100%;}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+  }
+  .router-fade-enter-active, .router-fade-leave-active {
+    transition: opacity .3s;
+  }
+  .router-fade-enter, .router-fade-leave-active {
+    opacity: 0;
+  }
+  .router-slid-enter-active, .router-slid-leave-active {
+    transition: all .4s;
+  }
+  .router-slid-enter, .router-slid-leave-active {
+    transform: translate3d(2rem, 0, 0);
+    opacity: 0;
+  }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import 'iview/dist/styles/iview.css';
+import './assets/css/my.less';
 
 Vue.config.productionTip = false;
 
