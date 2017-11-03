42116ac37e9728bc0bb09fc0e7c494c72b35a37d
qianqing <qian.qing@aliyun.com>
2017-11-03 18:06:03 +0800

    error page

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import './assets/css/my.less';
 
 Vue.config.productionTip = false;
+
+Vue.use(iView);
 
 /* eslint-disable no-new */
 new Vue({
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      path: '',
+      redirect: '/login',
+      meta: {requiresAuth: false}
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component(resolve) {
+        require.ensure(['../views/Login.vue'], () => {
+          resolve(require('../views/Login.vue'));
+        });
+      },
+      meta: {requiresAuth: false, title: '登录'}
     }
   ]
 });
