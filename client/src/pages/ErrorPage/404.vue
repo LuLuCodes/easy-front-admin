1952079bb0da58d8d60bbbf9c371b25000cc4423
qianqing <qian.qing@aliyun.com>
2017-11-06 17:20:33 +0800

    page title

--- client/src/pages/ErrorPage/404.vue
+++ client/src/pages/ErrorPage/404.vue
@@ -7,7 +7,7 @@
         <div class="error404-body-con">
             <Card>
                 <div class="error404-body-con-title">4<span><Icon type="ios-navigate-outline"></Icon></span>4</div>
-                <p class="error404-body-con-message">YOU&nbsp;&nbsp;LOOK&nbsp;&nbsp;LOST</p>
+                <p class="error404-body-con-message">很抱歉，您访问的页面不在地球上...</p>
                 <div class="error404-btn-con">
                     <Button @click="goHome" size="large" style="width: 200px;" type="text">返回首页</Button>
                     <Button @click="backPage" size="large" style="width: 200px;margin-left: 40px;" type="primary">返回上一页</Button>
@@ -26,7 +26,7 @@ export default {
         },
         goHome () {
             this.$router.push({
-                name: 'home_index'
+                name: 'home'
             });
         }
     }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -29,6 +29,33 @@ export default new Router({
         });
       },
       meta: {requiresAuth: false, title: '登录'}
+    },
+    {
+      path: '/401',
+      name: 'error_401',
+      component: resolve => { require(['../views/error_page/401.vue'], resolve); },
+      meta: {
+        title: '401-权限不足'
+      }
+    },
+    {
+      path: '/500',
+      name: 'error_500',
+      component: resolve => { require(['../views/error_page/500.vue'], resolve); },
+      meta: {
+        title: '500-服务端错误'
+      }
+    },
+    {
+      path: '*',
+      component(resolve) {
+        require.ensure(['../views/error_page/404.vue'], () => {
+          resolve(require('../views/error_page/404.vue'));
+        });
+      },
+      meta: {
+        title: '400-您的页面丢啦'
+      }
     }
   ]
 });
